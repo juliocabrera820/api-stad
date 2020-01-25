69f814ad09388fffc306b397d0188bf73a6fce0c
juliocabrera820 <juliocabrera820@gmail.com>
2020-01-25 17:53:19 -0600

    endpoints

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/juliocabrera/api-stad/controladores"
+)
+
+func main() {
+	router := gin.Default()
+
+	{
+		jugador := new(controladores.ControladorJugador)
+		router.POST("/jugadores", jugador.Agregar)
+		router.GET("/jugadores", jugador.Listar)
+		router.GET("/jugadores/:id", jugador.ObtenerJugador)
+		router.PUT("/jugadores/:id", jugador.Actualizar)
+		router.DELETE("/jugadores/:id", jugador.Eliminar)
+	}
+
+	router.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusNotFound, "No Encontrado")
+	})
+
+	router.Run(":9090")
+}
