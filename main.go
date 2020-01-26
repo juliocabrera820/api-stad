61d327ed75d4cbce63fc60cef83c5c8e7a60e125
juliocabrera820 <juliocabrera820@gmail.com>
2020-01-26 11:44:03 -0600

    nuevos endpoints

--- main.go
+++ main.go
@@ -16,6 +16,12 @@ func main() {
 		router.GET("/jugadores/:id", jugador.ObtenerJugador)
 		router.PUT("/jugadores/:id", jugador.Actualizar)
 		router.DELETE("/jugadores/:id", jugador.Eliminar)
+		equipo := new (controladores.ControladorEquipo)
+		router.POST("/equipos", equipo.Agregar)
+		router.GET("/equipos", equipo.Listar)
+		router.GET("/equipos/:id", equipo.ObtenerEquipo)
+		router.PUT("/equipos/:id", equipo.Actualizar)
+		router.DELETE("/equipos/:id", equipo.Eliminar)
 	}
 
 	router.NoRoute(func(c *gin.Context) {
